85dbdbfbbcfec9ac42f094f597c6e0be94de84b4
moahahaha <idahag23@vgs.nfk.no>
2024-06-04 15:49:47 +0200

    NEWW

--- Backend/backend.js
+++ Backend/backend.js
@@ -1,19 +1,107 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors')
-
+var mysql = require('mysql');
 const app = express();
 const PORT = 80;
+
+const fs = require('fs');
 
 app.use(cors())
 app.use(express.static('public'));
 app.use(express.json());
 
-var test = ["hei", "hallo", "lol"]
+
+var con = mysql.createConnection({
+    host: "10.0.0.156",
+    user: "ida_backend",
+    password: "Passord1",
+    database: "bibliotek_db"
+});
+
+
+function saveTestData(data) {
+    fs.writeFileSync('testData.json', JSON.stringify(data, null, 2), 'utf8');
+}
+
+function loadTestData() {
+    try {
+        const data = fs.readFileSync('testData.json', 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading test data file:', error);
+        return [];
+    }
+}
+
+app.get('/books', function (req, res) {
+
+    con.query("SELECT * FROM boker"
+    , function (err, result, fields) {
+      if (err) throw err;
+        console.log(result);
+        console.log('Sendt books')
+        res.json({
+         books: result
+        })
+     
+      });
+  });
+
+  app.post('/books', function (req, res) {
+    const { ISBN, Tittel, Publiseringsar, AvdelingID } = req.body;
+    const sql = "INSERT INTO boker (ISBN, Tittel, Publiseringsar, AvdelingID) VALUES (?, ?, ?, ?)";
+    con.query(sql, [ISBN, Tittel, Publiseringsar, AvdelingID], function (err, result) {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error adding book');
+      } else {
+        console.log('Book added', result);
+        res.status(201).send('Book added');
+      }
+    });
+  });
+
+  app.delete('/books/:ISBN', function (req, res) {
+    const ISBN = req.params.ISBN;
+    const sql = "DELETE FROM boker WHERE ISBN = ?";
+    con.query(sql, [ISBN], function (err, result) {
+        if (err) {
+            console.error(err);
+        res.status(500).send('Error deleting book')
+    }else {
+        console.log('Book deleted', result)
+        res.status(200).send('book deleted')
+    }
+    })
+  })
+
+
+
+var test = loadTestData();
 
 app.get('/test', async function (req, res) {
     res.json(test)
     
+})
+
+app.post('/test', function (req, res) {
+    const { testItem } = req.body; 
+    saveTestData(test);
+    test.push(testItem);
+    res.status(201).send('Test added');
+  });
+
+app.delete('/test/:test', function (req, res) {
+    const RemoveTest = req.params.test;
+    const index = test.indexOf(RemoveTest)
+    saveTestData(test);
+    if (index > -1) {
+        test.splice(index, 1);
+        res.status(200).send('Test deleted');
+      } else {
+        res.status(404).send('Test not found');
+      }
 })
 
 app.listen(PORT, () => {
--- test_vue_project_new/src/App.vue
+++ test_vue_project_new/src/App.vue
@@ -2,44 +2,177 @@
 <script>
 import { ref, onMounted } from 'vue';
 
-
 export default {
   setup() {
 
-  const testData = ref([]);
+    const testData = ref([]);
+    const newTest = ref('');
+    const books = ref([]);
+
+    const newBook = ref({ ISBN: '', Tittel: '', Publiseringsar: '', AvdelingID: '' });
 
 
-  async function get_test_data() {
-    try{
-      const response = await fetch('http://10.0.0.156:8080/test')
-      const data = await response.json()
-      testData.value = data.test
-      console.log(data)
-    } catch(error) {
-      console.error('There was an error fetching the data:', error)
+
+
+    async function fetchBooks() {
+      try {
+        const response = await fetch('http://10.0.0.156/books');
+        const data = await response.json();
+        books.value = data.books;
+        console.log("Books data:", data);
+      } catch (error) {
+        console.error('There was an error fetching the books:', error);
+      }
     }
-  }
-
-  onMounted(get_test_data) 
-
-  return {
-    testData,
-    get_test_data
-  }
-    
 
 
+
+    async function fetchTestData() {
+      try {
+        const response = await fetch('http://10.0.0.156/test');
+        const data = await response.json();
+        testData.value = data;
+        console.log("Test data:", data);
+      } catch (error) {
+        console.error('There was an error fetching the test data:', error);
+      }
     }
-}
+
+
+
+
+    async function addTest() {
+      try {
+        const response = await fetch('http://10.0.0.156/test', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ testItem: newTest.value })
+        });
+        if (response.ok) {
+          fetchTestData(); // Call fetchTestData to update the test list
+          newTest.value = '';
+        } else {
+          console.error('Failed to add test:', await response.text());
+        }
+      } catch (error) {
+        console.error('There was an error adding the test:', error);
+      }
+    }
+
+    async function deleteTest(test) {
+      try {
+        const response = await fetch(`http://10.0.0.156/test/${test}`, {
+          method: 'DELETE'
+        })
+        if (response.ok) {
+          fetchTestData();
+        } else {
+          console.error('Failed o delete test', await response.text())
+        }
+      } catch (error) {
+        console.error('There was an error deleting the task', error)
+      }
+    }
+
+
+    async function addBook() {
+      try {
+        const response = await fetch('http://10.0.0.156/books', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newBook.value)
+        });
+        if (response.ok) {
+          fetchBooks();
+          newBook.value = { ISBN: '', Tittel: '', Publiseringsar: '', AvdelingID: '' };
+        } else {
+          console.error('Failed to add book:', await response.text());
+        }
+      } catch (error) {
+        console.error('There was an error adding the book:', error);
+      }
+    }
+
+
+
+
+    async function deleteBook(bookISBN) {
+      try {
+        const response = await fetch(`http://10.0.0.156/books/${bookISBN}`, {
+          method: 'DELETE'
+        });
+        if (response.ok) {
+          fetchBooks();
+        } else {
+          console.error('Failed to delete book:', await response.text());
+        }
+      } catch (error) {
+        console.error('There was an error deleting the book:', error);
+      }
+    }
+
+
+
+    onMounted(() => {
+      fetchTestData();
+      fetchBooks();
+    });
+
+
+
+
+    return {
+      testData,
+      newTest,
+      addTest,
+      books,
+      newBook,
+      addBook,
+      deleteBook,
+      deleteTest
+    };
+  }
+};
 </script>
-
 
 <template>
   <div class="list-container">
-    <h2>Test Data List</h2>
-    <ul class="test-list">
-      <li v-for="test in testData" :key="test" class="test-item">{{ test }}</li>
-      
+
+
+    <h2>Bøker:</h2>
+
+    <input type="text" v-model="newBook.ISBN" placeholder="ISBN">
+    <input type="text" v-model="newBook.Tittel" placeholder="Enter Title">
+    <input type="text" v-model="newBook.Publiseringsar" placeholder="Publiseringsår">
+    <input type="text" v-model="newBook.AvdelingID" placeholder="Avdelings ID">
+
+    <button @click="addBook">Add Book</button>
+
+    <table>
+      <tr v-for="book in books" :key="book.ISBN">
+        <td>{{ book.ISBN }}</td>
+        <td>{{ book.Tittel }}</td>
+        <td>{{ book.Publiseringsar }}</td>
+        <td>{{ book.AvdelingID }}</td>
+        <td><button @click="deleteBook(book.ISBN)">Delete book</button></td>
+      </tr>
+    </table>
+
+    <br><br>
+
+    <h1>test</h1>
+    <input type="text" v-model="newTest" placeholder="Enter Test Item">
+
+    <button @click="addTest">Add Test</button>
+
+      <ul class="test-list">
+      <li v-for="test in testData" :key="test" class="test">{{ test }} <button @click="deleteTest(test)">Delete</button></li>
     </ul>
   </div>
+
+
 </template>
